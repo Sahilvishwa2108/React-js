0861680c6860d26244788cc3a76f15745f308f63
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-12-26 23:28:23 +0530

    adding some more features

---
+++ 07_reactRouter/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import Contact from "./Contact/Contact";
+import About from "./About/About";
+
+export {Header, Footer, Home, Contact, About};
--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './App.jsx'
 import './index.css'
+import {Home, About, Contact} from './components'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>} >
+      <Route path='' element={<Home />} />
+      <Route path='About' element={<About />} />
+      <Route path='Contact' element={<Contact />} />
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
