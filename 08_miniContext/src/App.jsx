157c4c7a31c4868870f0e2b72ec8afd6bd581557
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-12-29 01:03:06 +0530

    new commits

--- 08_miniContext/src/App.jsx
+++ 08_miniContext/src/App.jsx
@@ -1,11 +1,15 @@
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 import UserContextProvider from "./context/UserContextProvider"
+import "./App.css"
 
 function App() {
   
 
   return (
     <UserContextProvider>
-      
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
