5d838ce33958281ba365acd86803080909ecd295
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2024-01-10 23:46:25 +0530

    some new changes

--- megaBlogProject/src/components/index.js
+++ megaBlogProject/src/components/index.js
@@ -3,5 +3,27 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./Postcard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 
-export {Header,Footer,Container,Logo,LogoutBtn};
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Input
+};
